ee4d9fbe4567bae95d5256089e5e647cc3da8cea
phoenix2fire <abhishek.bussa@gmail.com>
2013-07-08 17:15:25 +0530

    code added

---
+++ Authentication.Proxy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Authentication.Proxy.ServiceReference1;
+using System.ServiceModel.Security;
+
+namespace Authentication.Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client proxy = new Service1Client();
+            proxy.ClientCredentials.UserName.UserName = "abc";
+            proxy.ClientCredentials.UserName.Password = "abc";
+            proxy.ClientCredentials.ServiceCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.None;
+            proxy.Open();
+            Console.WriteLine(proxy.GetData(1213));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Authentication.WCF.Library/CustomAuthentication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+
+namespace Authentication.WCF.Library
+{
+    public class CustomAuthentication: UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (!(userName.Equals("abc") && password.Equals("abc")))
+            {
+                throw new SecurityTokenException();
+            }
+        }
+    }
+}
